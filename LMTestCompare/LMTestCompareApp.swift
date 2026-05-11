92f19bd146d8c4e7145e711f486996606cc9ff58
Casey Fleser <cfleser@gmail.com>
2026-05-11 14:08:11 -0500

    Initial Commit

---
+++ LMTestCompare/LMTestCompareApp.swift
@@ -0,0 +1,17 @@
+//
+//  LMTestCompareApp.swift
+//  LMTestCompare
+//
+//  Created by Casey Fleser on 5/11/26.
+//
+
+import SwiftUI
+
+@main
+struct LMTestCompareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LMTestCompareTests/LMTestCompareTests.swift
@@ -0,0 +1,19 @@
+//
+//  LMTestCompareTests.swift
+//  LMTestCompareTests
+//
+//  Created by Casey Fleser on 5/11/26.
+//
+
+import Testing
+@testable import LMTestCompare
+
+struct LMTestCompareTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
